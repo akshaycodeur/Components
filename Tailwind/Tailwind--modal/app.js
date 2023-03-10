676fbc3fd346626687613946620f8c01f29e7750
Ashcode <akshaycodeur@gmail.com>
2023-03-10 12:20:33 -0500

    Restructure Tailwind folder

---
+++ Tailwind/Tailwind--modal/app.js
@@ -0,0 +1,6 @@
+let registerButton = document.querySelector('#registerNow')
+registerButton.addEventListener('click', showhideModal);
+
+function showhideModal(){
+    console.log('Show Hide modal');
+}
