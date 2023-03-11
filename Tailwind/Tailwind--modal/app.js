08647078c97796aa5640a56710d80befa10c8e19
Ashcode <akshaycodeur@gmail.com>
2023-03-11 09:53:24 -0500

    modal update

--- Tailwind/Tailwind--modal/app.js
+++ Tailwind/Tailwind--modal/app.js
@@ -1,6 +1,12 @@
 let registerButton = document.querySelector('#registerNow')
+let modalWindow = document.querySelector('#modalWindow')
+let modal = document.querySelector('#modal')
+let blackOverlay = document.querySelector('#modalWindow:not(#modal)')
+let modalClose = document.querySelector('#modalClose')
 registerButton.addEventListener('click', showhideModal);
 
 function showhideModal(){
-    console.log('Show Hide modal');
+    modalWindow.classList.toggle('hidden');
 }
+
+modalClose.addEventListener('click', showhideModal);
