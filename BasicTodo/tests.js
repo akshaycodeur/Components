c08ef2b332d2296e5ae44417fae0445bca0bd107
Ashcode <akshaycodeur@gmail.com>
2023-03-20 00:00:52 -0400

    edits

---
+++ BasicTodo/tests.js
@@ -0,0 +1,16 @@
+console.log(Math.pow(3,5));
+console.log(-8/-4);
+console.log(100%3);
+console.log((Math.pow(3,5))+(-8/-4)+(100%3));
+console.log('Hodor');
+console.log('- Did Joffrey agree?\n- He did. He also said "I love using \\n".');
+
+// - Did Joffrey agree?
+// - He did. He also said "I love using \n".
+
+// Write a program that calculates and prints sequentially (one value in each line) the values of the following mathematical expressions:
+
+// 3 to the power of 5
+// -8 divided by -4
+// the remainder of dividing 100 by 3
+// the sum of the three previous expressions
